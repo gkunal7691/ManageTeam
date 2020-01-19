227d0fabe67fe03aaf34183b45b6a255740f3995
gauravkunal <kunal.gaurav@blobcity.com>
2020-01-19 18:11:18 +0530

    comment functionality in task

--- src/app/routes/+standard-shared/components/task-modal/task-modal.component.ts
+++ src/app/routes/+standard-shared/components/task-modal/task-modal.component.ts
@@ -32,7 +32,6 @@ export class TaskModalComponent implements OnInit, OnChanges {
   @Input() userId: any;
   @Input() userList: any;
   @Input() dueDate: any;
-  //@Output() showTask = new EventEmitter();
   @Output() updateTaskList = new EventEmitter();
   @Input() task: any;
 
@@ -40,7 +39,6 @@ export class TaskModalComponent implements OnInit, OnChanges {
     console.log(this.task)
     this.ngOnInit();
     this.taskDate = this.dueDate;
-
     // if (!this.editBtn && this.taskForm) {
     //   this.taskForm.enable();
     //   this.taskForm.get('priority').setValue("normal");
@@ -379,5 +377,10 @@ export class TaskModalComponent implements OnInit, OnChanges {
       this.updateTaskList.emit();
     });
   }
+
+  updateTaskComment() {
+    this.updateTaskList.emit();
+  }
+
 
 }
