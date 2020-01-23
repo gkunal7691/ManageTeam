c867a0c69509bd1880614f26721e849e02ff13c7
Rajesh Maharana <rajesh.maharana@softobotics.com>
2020-01-24 00:23:04 +0530

    reset pwd done in emp side

--- src/app/services/forgot-password.service.ts
+++ src/app/services/forgot-password.service.ts
@@ -1,7 +1,8 @@
 import { Injectable, Inject } from '@angular/core';
 
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { CacheService } from './cache.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,19 +13,31 @@ export class ForgotPasswordService {
   private apiPath: string;
   private protocol:string;
   private host:string;
+  private users:string;
 
 
   constructor( 
-    private httpClient: HttpClient,
+    private httpClient: HttpClient, private cacheService: CacheService
   ) {
     this.protocol = document.location.protocol 
     this.host=document.location.host;
 
     const env: any = environment;
-
-    this.route = 'auth';
     this.apiPath = env.paths.api;
+    this.route = 'auth';
+    this.users = 'users';
+   
+
    }
+
+   getHeaders() {
+    return {
+      headers: new HttpHeaders({
+        'Authorization': `Bearer ${this.cacheService.getCache('user').token}`
+      })
+    }
+  }
+
 
    sendEmail(email:string,orgId:number) {
     return this.httpClient.delete<Object>(`${this.apiPath}/${this.route}/${email}/${orgId}`);
@@ -32,8 +45,8 @@ export class ForgotPasswordService {
 
   
 
-  resetPassword(profileData:Object){
-    return this.httpClient.patch<Object>(`${this.apiPath}/${this.route}/`, profileData);
+  resetPassword(profileData){
+    return this.httpClient.put<Object>(`${this.apiPath}/${this.users}/`, profileData,this.getHeaders());
   }
 
 }
