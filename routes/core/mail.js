d0fc368edc79a88af1adf72e83b63255c55b3fa1
Gaurav Kunal <40642673+gkunal7691@users.noreply.github.com>
2020-02-04 11:59:06 +0530

    mailer updated!

--- routes/core/mail.js
+++ routes/core/mail.js
@@ -2,11 +2,6 @@ const express = require('express');
 const router = express.Router();
 const nodemailer = require("nodemailer");
 
-//let testAccount = nodemailer.createTestAccount();
-
-// create reusable transporter object using the default SMTP transport
-
-
 router.post('/', async function (req, res, next) {
     console.log("hello");
     let transporter = nodemailer.createTransport({
@@ -14,8 +9,8 @@ router.post('/', async function (req, res, next) {
         port: 465,
         secure: true, // true for 465, false for other ports
         auth: {
-            user: 'gaurav.kunal@softobotics.com', // generated ethereal user
-            pass: 'Kunal@123' // generated ethereal password
+            user: 'notification@softobotics.com', // generated ethereal user
+            pass: 'notification@123' // generated ethereal password
         }
     });
 
@@ -37,16 +32,6 @@ router.post('/', async function (req, res, next) {
 
         }
     });
-
-    //console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-    // Preview only available when sending through an Ethereal account
-    //console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-
-
-    
 
 });
 
