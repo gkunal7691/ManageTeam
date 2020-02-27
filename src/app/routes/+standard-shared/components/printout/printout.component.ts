4460805718520c76ab38a1e1e4c2b0676d446792
Rajesh Maharana <rajesh.maharana@softobotics.com>
2020-02-27 14:54:18 +0530

    installed pdf-export

---
+++ src/app/routes/+standard-shared/components/printout/printout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-printout',
+  templateUrl: './printout.component.html',
+  styleUrls: ['./printout.component.scss']
+})
+export class PrintoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
